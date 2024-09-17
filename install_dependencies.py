6e227e4e3e03823550a30b1f9d70182b084baa95
Taylor <78652522+Taylor-eOS@users.noreply.github.com>
2024-09-17 07:50:40 +0200

    Install dependencies

---
+++ install_dependencies.py
@@ -0,0 +1,21 @@
+import os
+import subprocess
+import sys
+
+packages = {
+    "fitz": "PyMuPDF",
+    "wordfreq": "wordfreq",
+    "pygame": "pygame",
+    "numpy": "numpy",
+    "tensorflow": "tensorflow",
+    "sklearn": "scikit-learn",
+    "spacy": "spacy",
+    #"torch": "torch",
+    #"transformers": "transformers",
+}
+
+for module_name, package_name in packages.items():
+    try:
+        __import__(module_name)
+    except ImportError:
+        subprocess.check_call([sys.executable, "-m", "pip", "install", package_name])
